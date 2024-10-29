cddb1aca81a6ae1f8948559b4f4c4109a8315471
Mattea F <mattfoth04@gmail.com>
2024-10-29 20:34:40 +1100

    Index initial work - static carousel, spacing

--- a3/index.php
+++ a3/index.php
@@ -15,7 +15,33 @@ require("include/db_connect.inc"); // placed here to show connection issues on l
     ?>
 
     <header>
-        <h1 class="index"><img src="images/main.jpg" alt="Main image" class="main">PETS VICTORIA
+        <h1 class="index">
+            
+        <!-- <img src="images/dog4.jpeg" alt="Main image" class="frontimage main"> -->
+        
+        <div id="carouselExample" class="carousel slide carouselWidth">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/dog4.jpeg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/dog3.jpeg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/dog2.jpeg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+        PETS VICTORIA
         </h1>
         <h2 class="index">
             WELCOME TO PET<br>
