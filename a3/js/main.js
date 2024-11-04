1126f4c4bdb14f9ecbbe6634622cf47c2321e446
Mattea F <mattfoth04@gmail.com>
2024-11-04 16:03:54 +1100

    Nav Bar rework - no dropdown, highlight current page, login/logged-out differenciation

--- a3/js/main.js
+++ a3/js/main.js
@@ -1,16 +1,16 @@
-function dropdownDirect() {
-    const selectedValue = document.getElementById("navigation").value;
+// function dropdownDirect() {
+//     const selectedValue = document.getElementById("navigation").value;
 
-    if (selectedValue == "home"){
-        window.location.href = './index.php';
-    }
-    else if (selectedValue == "pets"){
-        window.location.href = './pets.php';
-    }    
-    else if (selectedValue == "add"){
-        window.location.href = './add.php';
-    }    
-    else if (selectedValue == "gallery"){
-        window.location.href = './gallery.php';
-    }
-}
+//     if (selectedValue == "home"){
+//         window.location.href = './index.php';
+//     }
+//     else if (selectedValue == "pets"){
+//         window.location.href = './pets.php';
+//     }    
+//     else if (selectedValue == "add"){
+//         window.location.href = './add.php';
+//     }    
+//     else if (selectedValue == "gallery"){
+//         window.location.href = './gallery.php';
+//     }
+// }
